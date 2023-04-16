45e8be770d84990250c13bda742494804a3e1518
thikimthuy51 <20522004@gm.uit.edu.vn>
2023-04-16 11:10:57 +0700

    edit profile

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,10 +1,30 @@
 import React from "react";
-import "../styles/SideMenu.css";
-// import {SideMenuData} from "./SideMenuData";
+import "../styles/Profile.css";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {SideMenuData} from "./SideMenuData";
 function SideMenu() {
   return (
-    <div className="SideMenu">
-      <p>hi</p>
+    <div className="SideMenu"> 
+      <ul className="SideMenuList">
+        <div className="title">
+          <AccountCircleIcon className="accountCircleIcon" fontSize="large"/>
+          <div>
+            <h3>Xin chào Quý!</h3>
+            <p>Chỉnh sửa thông tin</p>
+          </div>
+        </div>
+        {SideMenuData.map((val,key) => {
+          return (
+            <li className="row" key = {key} onClick={() => {window.location.pathname = val.link;
+            }}
+            >
+              {""}
+              <div className="content">{val.icon}</div>
+              <div>{val.title}</div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
---
+++ src/components/SideMenuData.js
@@ -0,0 +1,31 @@
+import React from "react";
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import PersonPinCircleIcon from '@material-ui/icons/PersonPinCircle';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+export const SideMenuData = [
+    {
+        title: "Sản phẩm yêu thích",
+        icon: <FavoriteBorderIcon/>,
+        link:"/"
+    },
+    {
+        title: "Quản lý sổ địa chỉ",
+        icon: <PersonPinCircleIcon/>,
+        link:"/"
+    },
+    {
+        title: "Đơn mua",
+        icon: <ListAltIcon/>,
+        link:"/"
+    },
+    {
+        title: "Kho Voucher",
+        icon: <BookmarkBorderIcon/>,
+        link:"/"
+    },
+];
+    
+
+    
+
