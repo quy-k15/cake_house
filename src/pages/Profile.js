2a21e269deb1413f92a098cce075f9d26d269dbf
thikimthuy51 <20522004@gm.uit.edu.vn>
2023-04-25 10:07:16 +0700

    edit Profile_info

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,23 +3,33 @@ import PizzaLeft from "../assets/pizzaLeft.jpg";
 import "../styles/Profile.css";
 import SideMenu from "../components/SideMenu";
 function Profile() {
-  return (
-      <div className="contact">
-          <div className="leftSide">
-              <SideMenu />
-          </div>
-          <div className="rightSide">
-              
+    return (
+        <div className="contact">
+            <div className="leftSide">
+                <SideMenu />
+            </div>
+            <div className="rightSide">
 
-              <form id="contact-form" method="POST">
-                  <h3>Thông tin tài khoản</h3>
-                  <p>Trần Văn Quý</p>
-                  <p>Email: 20521818@gm.uit.edu.vn</p>
-                  <p>Số điện thoại: 0889201726</p>
-                  <p>Giới tính: Nam</p>
-                  <p>Ngày sinh: 08/01/2002</p>
+                <div>
+                    <div className="contact-form" method="POST">
+                        <div className="info-user">
+                            <h3>Thông tin tài khoản</h3>
+                            <p>Trần Văn Quý</p>
+                            <p>Email: 20521818@gm.uit.edu.vn</p>
+                            <p>Số điện thoại: 0889201726</p>
+                            <p>Giới tính: Nam</p>
+                            <p>Ngày sinh: 08/01/2002</p>
+                        </div>
+                        <div className="avarta" >
+                        <img></img>
+                            <button>
+                                Chọn ảnh
+                            </button>
 
-                  <label htmlFor="name">Full Name</label>
+                        </div>
+
+
+                        {/* <label htmlFor="name">Full Name</label>
                   <input name="name" placeholder="Enter full name..." type="text" />
                   <label htmlFor="email">Email</label>
                   <input name="email" placeholder="Enter email..." type="email" />
@@ -30,11 +40,24 @@ function Profile() {
                       name="message"
                       required
                   ></textarea>
-                  <button type="submit"> Send Message</button>
-              </form>
-          </div>
-      </div>
-  );
+                  <button type="submit"> Send Message</button> */}
+                    </div>
+
+
+
+                </div>
+
+                <div className="address">
+                <h3>Sổ địa chỉ</h3>
+
+
+                </div>
+
+
+
+            </div>
+        </div>
+    );
 }
 
 export default Profile;
