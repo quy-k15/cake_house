34ee19345437a4b491fc072d88ad8ebfef7375ee
nhungthaithi66 <20521726@gm.uit.edu.vn>
2023-05-02 20:10:53 +0700

    loginform

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,74 +1,69 @@
-import React from "react";
+// import React from "react";
 import CoverImg from "../assets/CoverImg.jpg";
 import GoogleIcon from "../assets/GoogleIcon.jpg"
-<link ref="stylesheet" href="./build/tailwind.css"></link>
+import "../styles/Login.css";
+//<link ref="stylesheet" href="./build/tailwind.css"></link>
 const colors = {
     primary: "#060606",
     background: "#E0E0E0",
     disabled: "#D9D9D9"
 }
-
 const Login= ()=> {
     return (
-        <div className="w-full h-screen flex items-start">
-            <div className='relative w-1/2 h-full flex flex-col'>
-                <div className = 'absolute top-[20%] left-[10%] flex flex-col'>
-                    <h1 className='text-4xl text-white font-bold my-4'>Let's shopping your favourite cake!</h1>
-                    <p className='text-xl text-white font-normal'>For our desserts we always use only the natural ingredients. Our production is 100% handmade</p>
+        <div className="screen">
+            <div className="leftScreen">
+                <div className = "leftText">
+                    <h1>Let's shopping your favourite cake!</h1>
+                    <p>For our desserts we always use only the natural ingredients. Our production is 100% handmade</p>
                 </div>
-                <img src = {CoverImg} className = "w-full h-full object-cover" />
+                <img src = {CoverImg} />
             </div>
 
-            <div className='w-1/2 h-full bg-[#f5f5f5] flex flex-col p-20 justify-between items-center'>
-                <h1 className='w-full max-w-[500px] mx-auto text-x1 text-[#060606] font-semibold mr-auto'>Mom's Cake</h1>
+            <div className="rightScreen">
+                <h1 className="header">Mom's Cake</h1>
 
-                <div className='w-full flex flex-col max-w-[500px]'>
-                    <div className='w-full flex flex-col mb-2'>
-                        <h3 className='text-3xl font-semibold mb-2'>Login</h3>
-                        <p className='text-base mb-2'>Welcome Back! Please enter your details.</p>
+                <div className="loginField">
+                    <div className="loginHeader">
+                        <h3>Login</h3>
+                        <p>Welcome Back! Please enter your details.</p>
                     </div>
 
-                    <div className='w-full flex flex-col'>
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            className='w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none' />
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            className='w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none' />
-                    </div>
+                    <form>
+                        <input placeholder="Email" type="email" />
 
-                    <div className='w-full flex items-center justify-between'>
-                        <div className='w-full flex items-center'>
+                        <input placeholder="Password" type="password" />
+                    </form>
+
+                    <div className="loginExten">
+                        <div className="loginExtenSub">
                             <input type="checkbox" className="w-4 h-4 mr-2" />
                             <p className='text-sm'>Remember me for 30 days</p>
                         </div>
 
-                        <p className='text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2'>Forgot Password?</p>
+                        <p className="forgot">Forgot Password?</p>
                     </div>
 
-                    <div className='w-full flex flex-col my-4'>
-                        <button className='w-full text-white my-2 font-semibold bg-[#060606] rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>
+                    <div className="buttonField">
+                        <button>
                             Log in
                         </button>
-                        <button className='w-full text-[#060606] my-2 font-semibold bg-white border border-black rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>
+                        <button>
                             Register
                         </button>
                     </div>
 
-                    <div className='w-full flex items-center justify-center relative py-2'>
-                        <div className='w-full h-[1px] bg-black/40'></div>
-                        <p className='text-lg absolute text-black/80 bg-[#f5f5f5]'>or</p>
+                    <div className="option2">
+                        <div className="line"></div>
+                        <p>or</p>
                     </div>
 
-                    <div className='w-full text-[#060606] my-2 font-semibold bg-white border border-black/40 rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>
-                        <img src={GoogleIcon} className="h-6 mr-2" />
+                    <div className="google">
+                        <img src={GoogleIcon} className="icon" />
                         Sign in With Google
                     </div>
 
-                    <div className='w-full flex items-center justify-center'>
-                        <p className='text-sm font-normal text-[#060606]'>Don't have account? <span className='font-semibold underline underline-offset-2 cursor-pointer'>Sign up</span></p>
+                    <div className="loginFooter">
+                        <p>Don't have account? <span>Sign up</span></p>
                     </div>
                 </div>
             </div>
