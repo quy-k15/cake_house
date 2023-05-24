a04750e889f99bb15ad5f360088dfe712046ae17
nhungthaithi66 <20521726@gm.uit.edu.vn>
2023-05-24 22:51:45 +0700

    aboutusdone

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Theme from "../assets/CoverPic.jpg";
-import Cake from "../assets/cakeab.jpg";
+import Cake from "../assets/AB.png";
 import Baker1 from "../assets/bakerman.png";
 import Baker2 from "../assets/bakergirl.png";
 import "../styles/About.css";
@@ -23,26 +23,26 @@ function About() {
           <img src = {Cake} />
         </div>
         <div className="rightBetween">
-          <h3>Chuyên cung cấp các loại bánh ngọt</h3>
+          <h3 class="tieude">Chuyên cung cấp các loại bánh ngọt</h3>
           <p className="textab">
-          Nhận đặt làm theo yêu cầu của kháhc hàng. scabScvbuxiakbkxb ksdksd 
-          Các thành phần bánh được sử dụng phổ biến nhất bao gồm bột, đường, trứng, bơ hoặc dầu hoặc bơ thực vật, một chất lỏng và các chất men, như baking soda hoặc bột nở. 
-          Các thành phần bổ sung và hương liệu phổ biến bao gồm đồ khô, kẹo, hoặc trái cây tươi, các loại hạt, ca cao và chiết xuất như vani, với nhiều thay thế cho các thành phần chính. 
-          Bánh cũng có thể được làm đầy với chất bảo quản trái cây, các loại hạt hoặc các loại sốt tráng miệng (như kem bánh ngọt), đá lạnh với kem bơ hoặc các loại kem khác, và được trang trí với bánh hạnh nhân (marzipan), đường viền trang trí bánh (piped borders) hoặc kẹo trái cây
+            Nhận đặt làm theo yêu cầu của khách hàng.<br />
+            Các thành phần bánh được sử dụng phổ biến nhất bao gồm bột, đường, trứng, bơ hoặc dầu hoặc bơ thực vật, một chất lỏng và các chất men, như baking soda hoặc bột nở.<br />
+            <br></br>
+            Các thành phần bổ sung và hương liệu phổ biến bao gồm đồ khô, kẹo, hoặc trái cây tươi, các loại hạt, ca cao và chiết xuất như vani, với nhiều thay thế cho các thành phần chính.<br />
+            Bánh cũng có thể được làm đầy với chất bảo quản trái cây, các loại hạt hoặc các loại sốt tráng miệng (như kem bánh ngọt), đá lạnh với kem bơ hoặc các loại kem khác, và được trang trí với bánh hạnh nhân (marzipan), đường viền trang trí bánh (piped borders) hoặc kẹo trái cây.
           </p>
         </div>
       </div>
       <div class="team" id="team">
         <h1 class="heading">our  <span>team</span></h1>
         <div class="box-container">
-
             <div class="box">
-                <div class="image">
+                <div class="imagestaff">
                   <img src = {Baker1} />
                 </div>
                 <div class="content">
-                    <h3>erica lacy</h3>
-                    <p>CEO</p>
+                    <h3 class="staffname">erica lacy</h3>
+                    <p class="position">CEO</p>
                     <div class="share">
                         <i class="fab fa-facebook-f"></i>
                         <i class="fab fa-twitter"></i>
@@ -52,12 +52,12 @@ function About() {
             </div>
 
             <div class="box">
-                <div class="image">
+                <div class="imagestaff">
                   <img src = {Baker2} />
                 </div>
                 <div class="content">
-                    <h3>doe lacy</h3>
-                    <p>manager</p>
+                    <h3 class="staffname">doe lacy</h3>
+                    <p class="position">manager</p>
                     <div class="share">
                         <i class="fab fa-facebook-f"></i>
                         <i class="fab fa-twitter"></i>
@@ -67,12 +67,12 @@ function About() {
             </div>
 
             <div class="box">
-                <div class="image">
+                <div class="imagestaff">
                   <img src = {Baker2} />
                 </div>
                 <div class="content">
-                    <h3>john lacy</h3>
-                    <p>manager</p>
+                    <h3 class="staffname">john lacy</h3>
+                    <p class="position">manager</p>
                     <div class="share">
                         <i class="fab fa-facebook-f"></i>
                         <i class="fab fa-twitter"></i>
@@ -81,7 +81,7 @@ function About() {
                 </div>
             </div>
 
-        </div>
+      </div>
 
     </div>
 
