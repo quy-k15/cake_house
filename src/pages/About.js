1af0313daed46c0f2b2fff9a322d44f6deef6543
nhungthaithi66 <20521726@gm.uit.edu.vn>
2023-07-07 19:56:15 +0700

    about_us and feedback

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,11 +11,11 @@ function About() {
         <img src = {Theme} />
       </div>
       <div className="aboutBottom">
-        <h1 class="heading"> <span>about</span> us </h1>
+        <h1 class="heading"> <span>về</span> chúng tôi </h1>
         <div class="content">
                 <h3>Good things come to those <span>who bake </span> for others</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit veritatis sunt id modi quis in eveniet at! Vero iusto excepturi.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam molestiae maxime quibusdam rem necessitatibus optio maiores fugiat. Pariatur molestiae.</p>
+                <p>Chào mừng đến với website bán bánh của chúng tôi! Chúng tôi rất vui được giới thiệu về dịch vụ và sản phẩm mà chúng tôi cung cấp.</p>
+                <p>Chúng tôi là một đội ngũ đam mê bánh ngọt và cam kết mang đến cho khách hàng những trải nghiệm tuyệt vời với các loại bánh ngon và đa dạng. Chất lượng là tiêu chí hàng đầu của chúng tôi, và chúng tôi cam kết chỉ sử dụng những nguyên liệu tươi ngon, tự nhiên và an toàn cho sức khỏe.</p>
         </div>
       </div>
       <div className="between">
@@ -25,11 +25,11 @@ function About() {
         <div className="rightBetween">
           <h3 class="tieude">Chuyên cung cấp các loại bánh ngọt</h3>
           <p className="textab">
-            Nhận đặt làm theo yêu cầu của khách hàng.<br />
-            Các thành phần bánh được sử dụng phổ biến nhất bao gồm bột, đường, trứng, bơ hoặc dầu hoặc bơ thực vật, một chất lỏng và các chất men, như baking soda hoặc bột nở.<br />
-            <br></br>
-            Các thành phần bổ sung và hương liệu phổ biến bao gồm đồ khô, kẹo, hoặc trái cây tươi, các loại hạt, ca cao và chiết xuất như vani, với nhiều thay thế cho các thành phần chính.<br />
-            Bánh cũng có thể được làm đầy với chất bảo quản trái cây, các loại hạt hoặc các loại sốt tráng miệng (như kem bánh ngọt), đá lạnh với kem bơ hoặc các loại kem khác, và được trang trí với bánh hạnh nhân (marzipan), đường viền trang trí bánh (piped borders) hoặc kẹo trái cây.
+            Trên trang web của chúng tôi, bạn sẽ tìm thấy một bộ sưu tập đa dạng các loại bánh, từ bánh mousse nhẹ nhàng, bánh kem truyền thống, đến bánh cupcake ngon lành và bánh tart tươi ngon. Chúng tôi cũng cung cấp các loại bánh dành cho các dịp đặc biệt như sinh nhật, kỷ niệm, hay đám cưới.<br />
+          <br></br>
+            Đội ngũ chúng tôi là những người lành nghề, giàu kinh nghiệm và sáng tạo. Chúng tôi không chỉ tạo ra những món bánh ngon mắt mà còn đặc biệt chú trọng vào việc tạo ra những thiết kế bắt mắt và độc đáo. Chúng tôi luôn cố gắng đáp ứng các yêu cầu đặc biệt của khách hàng và tạo ra những món bánh độc đáo và đáng nhớ.<br />
+          <br></br>
+            Ngoài ra, chúng tôi cũng đảm bảo rằng quy trình đặt hàng và giao hàng của chúng tôi đơn giản và thuận tiện. Bạn có thể dễ dàng chọn món bánh yêu thích và đặt hàng trực tuyến, và chúng tôi sẽ giao hàng tận nơi trong thời gian ngắn nhất.
           </p>
         </div>
       </div>
@@ -44,9 +44,11 @@ function About() {
                     <h3 class="staffname">tran van quy</h3>
                     <p class="position">CEO</p>
                     <div class="share">
+                      <a href="https://www.facebook.com/han.ni.35380" target="_blank">
                         <i class="fab fa-facebook-f"></i>
-                        <i class="fab fa-twitter"></i>
-                        <i class="fab fa-instagram"></i>
+                      </a>
+                      <i class="fab fa-twitter"></i>
+                      <i class="fab fa-instagram"></i>
                     </div>
                 </div>
             </div>
@@ -59,9 +61,13 @@ function About() {
                     <h3 class="staffname">thai thi nhung</h3>
                     <p class="position">manager</p>
                     <div class="share">
+                      <a href="https://www.facebook.com/nhungthaithi239/" target="_blank">
                         <i class="fab fa-facebook-f"></i>
-                        <i class="fab fa-twitter"></i>
+                      </a>
+                      <i class="fab fa-twitter"></i>
+                      <a href="https://www.instagram.com/nhungthaithi_/" target="_blank">
                         <i class="fab fa-instagram"></i>
+                      </a>
                     </div>
                 </div>
             </div>
@@ -74,9 +80,11 @@ function About() {
                     <h3 class="staffname">nguyen thi kim thuy</h3>
                     <p class="position">manager</p>
                     <div class="share">
+                      <a href="https://www.facebook.com/profile.php?id=100009301584614" target="_blank">
                         <i class="fab fa-facebook-f"></i>
-                        <i class="fab fa-twitter"></i>
-                        <i class="fab fa-instagram"></i>
+                      </a>
+                      <i class="fab fa-twitter"></i>
+                      <i class="fab fa-instagram"></i>
                     </div>
                 </div>
             </div>
--- src/pages/CompletedPurchase.js
+++ src/pages/CompletedPurchase.js
@@ -1,86 +1,143 @@
-import React from "react";
+import React, { useState } from "react";
 import NavbarOrders from "../components/NavbarOrders";
 import SideMenu from "../components/SideMenu";
-import {OrdersData} from "../components/OrdersData";
+import { OrdersData } from "../components/OrdersData";
+import { faStar as solidStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as lightStar } from "@fortawesome/free-regular-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../styles/Rated.css";
 
 
 function CompletedPurchase() {
-    return (
-        <div className="MyOrders">
-            <div className="leftSide">
-                <SideMenu />
-            </div>
-            <div className="rightSide">
-                <NavbarOrders/>
-                <div className="all_purchase">
-                {OrdersData.map((val, key) => {
-                        return (
-                            <div
-                                className="row"
-                                key={key}
-                                id={window.location.pathname == val.link ? "active" : ""}
-                                onClick={() => {
-                                    window.location.pathname = val.link;
-                                }}
-                            >
-                                {""}
-                                <div className="ruler_status">
-                                    <div id="id">
-                                        {val.id}
-                                        <div id="id_inf">123456789</div>
-                                    </div>
-                                    
-                                    <div id="tradeDate">
-                                        {val.tradeDate}
-                                        <div id="tradeDate_inf">12/06/2023</div>
-                                    </div>
-                                    <div id="status">
-                                        {val.receivingdate}
-                                        <div id="status_inf">13/06/2023</div>
-                                    </div>
-                                    <div >
-                                        <button id="btn_detail">
-                                            Xem chi tiết <i class="fa-solid fa-arrow-right"></i>
-                                        </button>
-                                    </div>
-                                    
-                                </div>
-                                
-                                <div className="detail_purchase">
-                                    <div className="col1">
-                                        <div id="image">{val.image}</div>
-                                    </div>
-                                    <div className="col2">
-                                        <div id="name">{val.name}</div>
-                                        <div id="unit_price">
-                                            {val.unit_price}
-                                            <div id="unit_price_inf">100.000 (VND)</div>
-                                        </div>
-                                        <div id="quantity">
-                                            {val.quantity}
-                                            <div id="quantity_inf">02</div>
-                                        </div>
-                                    </div>
-                                    <div className="col3">
-                                        <div id="total_price">
-                                            {val.total_price}
-                                            <div id="total_price_inf">200.000 (VND)</div>
-                                        </div>
-                                        <button className="buyagain">Mua lại</button>
-                                        <button className="rate">Đánh giá</button>
-                                    </div>
-                                    
-                                </div>
-                                
-                            </div>
-                        );
-                    })}
+  const [rating, setRating] = useState(0);
+  const [showPopup, setShowPopup] = useState(false);
+  
+  const handleRatingChange = (value) => {
+    setRating(value);
+  };
+  
+  const handleRateClick = () => {
+    setShowPopup(true);
+  };
+  const handleSubmit = () => {
+    // Perform the desired action here, such as sending the review to the server
+    // You can access the rating and review values from the component's state
+  
+    // After submitting the review, you can close the popup or reset the form
+    closePopup();
+  };
+  
+  const closePopup = () => {
+    setShowPopup(false);
+  };
 
+  return (
+    <div className="MyOrders">
+      <div className="leftSide">
+        <SideMenu />
+      </div>
+      <div className="rightSide">
+        <NavbarOrders />
+        <div className="all_purchase">
+          {OrdersData.map((val, key) => {
+            return (
+                <div
+                className="row"
+                key={key}
+                id={window.location.pathname === val.link ? "active" : ""}
+                onClick={() => {
+                    if (val.link) {
+                    window.location.pathname = val.link;
+                    }
+                }}
+                >
+                <div className="ruler_status">
+                  <div id="id">
+                    {val.id}
+                    <div id="id_inf">123456789</div>
+                  </div>
+
+                  <div id="tradeDate">
+                    {val.tradeDate}
+                    <div id="tradeDate_inf">12/06/2023</div>
+                  </div>
+                  <div id="status">
+                    {val.receivingdate}
+                    <div id="status_inf">13/06/2023</div>
+                  </div>
+                  <div>
+                    <button id="btn_detail">
+                      Xem chi tiết <i className="fa-solid fa-arrow-right"></i>
+                    </button>
+                  </div>
                 </div>
-            </div>
-           
+
+                <div className="detail_purchase">
+                  <div className="col1">
+                    <div id="image">{val.image}</div>
+                  </div>
+                  <div className="col2">
+                    <div id="name">{val.name}</div>
+                    <div id="unit_price">
+                      {val.unit_price}
+                      <div id="unit_price_inf">100.000 (VND)</div>
+                    </div>
+                    <div id="quantity">
+                      {val.quantity}
+                      <div id="quantity_inf">02</div>
+                    </div>
+                  </div>
+                  <div className="col3">
+                    <div id="total_price">
+                      {val.total_price}
+                      <div id="total_price_inf">200.000 (VND)</div>
+                    </div>
+                    <button className="buyagain">Mua lại</button>
+                    <button className="rate" onClick={handleRateClick}>
+                      Đánh giá
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
-    );
+      </div>
+
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <h3>Đánh giá sản phẩm</h3>
+            <div className="star_rate">
+              <div className="rating-section">
+                <label htmlFor="rating">Chất lượng:</label>
+              </div>
+              <div className="star-rating-section">
+                {[1, 2, 3, 4, 5].map((index) => (
+                  <span
+                    key={index}
+                    onClick={() => handleRatingChange(index)}
+                    className={`star ${index <= rating ? "active" : ""}`}
+                  >
+                    <FontAwesomeIcon icon={index <= rating ? solidStar : lightStar} />
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="review-section">
+              <label htmlFor="review">Đánh giá:</label>
+              <textarea className="review_fb" id="review" rows="4" />
+            </div>
+            <div className="popup-button">
+              <button className="update-button" onClick={handleSubmit}>Đánh giá</button>
+              <button className="close-button" onClick={closePopup}>Đóng</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default CompletedPurchase;
