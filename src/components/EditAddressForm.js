05eb9ef98336b9dca4bb919496c5933dc7b08003
Quý <80635501+quy-k15@users.noreply.github.com>
2023-07-08 11:58:03 +0700

    Thêm địa chỉ mới vô firebase

--- src/components/EditAddressForm.js
+++ src/components/EditAddressForm.js
@@ -6,6 +6,11 @@ import * as Yup from 'yup';
 import Location from "../components/Location";
 import { Label } from '@material-ui/icons';
 import "../styles/AddressBook.css";
+import { collection,addDoc , getDocs,getDoc,updateDoc, doc,query, where, } from 'firebase/firestore/lite'; 
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { useState,useEffect } from "react";
+
 
 const EditAddressForm = () => {
   // const paperStyle = { padding: '0 15px 40px 15px', width: 200 };
@@ -15,6 +20,9 @@ const EditAddressForm = () => {
   const initialValues = {
     name: '',
     phoneNumber: '',
+    province: '',
+    district: '',
+    ward: '',
   };
   const validationSchema = Yup.object().shape({
     name: Yup.string().min(3, "It's too short").required("Required"),
@@ -29,7 +37,93 @@ const EditAddressForm = () => {
     alert(JSON.stringify(values, null, 2));
     props.resetForm();
   };
+  // const handleLocationChange = (values, setFieldValue) => {
+  //   // Update the state with the selected location values
+  //   setFieldValue('province', values.province);
+  //   setFieldValue('district', values.district);
+  //   setFieldValue('ward', values.ward);
+  //   console.log("setFieldValue",setFieldValue);
+  // };
+  const [locationData, setLocationData] = useState(null);
+  const handleLocationChange = (locationData) => {
+    const { province, district, ward, wardText } = locationData;
+    console.log('Selected Province:', province);
+    console.log('Selected District:', district);
+    console.log('Selected Ward:', ward);
+    console.log('Selected Ward Text:', wardText);
+  };
+// Thêm vô firebase
+const { user } = UserAuth();
+    const [idUser, setIdUser] = useState('');
+    // console.log("user_id",user&&user.idUser);
+    const [email,setEmail]  = useState('');
+    const [userinfo,setUser]  = useState();
 
+    // const SetEmails = () => {
+    //     setEmail(user&&user.idUser);
+    // };
+   
+    useEffect(() => {
+        if (user) {
+          setEmail(user.email);
+        }
+      }, [user]);
+    
+    const UserQuery = async () => {
+      
+        const q = query(collection(db, "users"), where("email", "==", email));
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+            const doc = querySnapshot.docs[0];
+            setUser(doc.data());
+            console.log("userinfo: ", userinfo);
+        }
+    };
+    useEffect(() => {
+        if (email) {
+          UserQuery();
+        }
+      }, [email]);
+
+      const handleAddAddress = async (props, locationData) => {
+        try {
+            // UserQuery();
+            // if (!userinfo) {
+            //     setShowNotiLogin(true);
+            //     setTimeout(() => {
+            //         setShowNotiLogin(false);
+            //     }, 3000);
+            //     return;
+            //   }
+
+            const { name, phoneNumber } = props.values;
+            const { province, district, ward } = locationData;
+            const newAddress = {
+     
+                idAddress: "",
+                iduser: userinfo.idUser,
+                name:name,
+                phoneNumber:phoneNumber,
+                province:province,
+                district:district,
+                ward:ward,
+                used:false
+            };
+                const AddressCol = collection(db, "Address");
+                const docRef = await addDoc(AddressCol, newAddress);
+                const generatedId = docRef.id;
+                await updateDoc(doc(db, "Address", generatedId), {  idAddress: generatedId });
+
+                console.log("cart created successfully!");
+        } catch (error) {
+            console.error("Error creating order:", error);
+        }
+        // setShowNoti(true);
+        // setTimeout(() => {
+        //     setShowNoti(false);
+        // }, 3000);
+    };
+  
   return (
     <Grid className='EditAddress'>
       <Paper elevation={0}>
@@ -53,9 +147,20 @@ const EditAddressForm = () => {
                   error={props.errors.phoneNumber && props.touched.phoneNumber}
                   helperText={<ErrorMessage name='phoneNumber' />} required />
               </div>
-              <Location {...props} />
+               
+              {/* <Location {...props} /> */}
+              <Location
+              values={props.values}
+              setFieldValue={props.setFieldValue}
+              initialProvinces={[]}
+              initialDistricts={[]}
+              initialWards={[]}
+              // onLocationChange={handleLocationChange}
+              onLocationChange={(locationData) => setLocationData(locationData)}
+            />
 
-              <Button type='submit' style={btnStyle} variant='contained' color='primary'>Lưu</Button>
+
+            <Button type='submit' style={btnStyle} variant='contained' color='primary' onClick={() => handleAddAddress(props, locationData)}>Lưu</Button>
             </Form>
           )}
         </Formik>
--- src/components/Location.js
+++ src/components/Location.js
@@ -6,11 +6,29 @@ import "../styles/AddressBook.css";
 
 
 
-const Location = ({ values, setFieldValue }) => {
+
+const Location = ({
+  values,
+  setFieldValue,
+  initialProvinces,
+  initialDistricts,
+  initialWards,
+  onLocationChange
+}) => {
   const [provinces, setProvinces] = useState([]);
   const [districts, setDistricts] = useState([]);
   const [wards, setWards] = useState([]);
-  
+  const [provinceName, setProvinceName] = useState('');
+  const [districtName, setDistrictName] = useState('');
+  const [wardValue, setWardValue] = useState('');
+  // const [provinces, setProvinces] = useState([]);
+  // const [districts, setDistricts] = useState([]);
+  // const [wards, setWards] = useState([]);
+  // const getlocation = async () => {
+  //   setProvincesname(provinces.name);
+    
+
+  // }
 
   useEffect(() => {
     fetchProvinces();
@@ -21,6 +39,7 @@ const Location = ({ values, setFieldValue }) => {
       .get('https://provinces.open-api.vn/api/')
       .then((response) => {
         setProvinces(response.data);
+        // console.log("provinces",provinces)
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -33,6 +52,7 @@ const Location = ({ values, setFieldValue }) => {
       .then((response) => {
         const { districts } = response.data;
         setDistricts(districts);
+        // console.log("districts",districts)
         setWards([]); // Reset the wards when changing the district
       })
       .catch((error) => {
@@ -40,48 +60,163 @@ const Location = ({ values, setFieldValue }) => {
       });
   };
 
-  const fetchWards = (districtId) => {
-    axios
-      .get(`https://provinces.open-api.vn/api/districts/${districtId}/wards`)
-      .then((response) => {
-        setWards(response.data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  };
+  // const fetchWards = (districtId) => {
+  //   axios
+  //     .get(`https://provinces.open-api.vn/api/districts/${districtId}/wards`)
+  //     .then((response) => {
+  //       setWards(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error);
+  //     });
+  // };
 
   const handleProvinceChange = (e) => {
     const selectedProvinceId = e.target.value;
+    const selectedProvince = provinces.find((province) => province.code.toString() === selectedProvinceId.toString());
+    
     setFieldValue('province', selectedProvinceId);
     setFieldValue('district', '');
     setFieldValue('ward', '');
     setDistricts([]);
     setWards([]);
-
+    // console.log("selectedProvinceId",selectedProvinceId)
+    // console.log("selectedProvince",selectedProvince)
+    
     if (selectedProvinceId) {
       fetchDistricts(selectedProvinceId);
+      setProvinceName(selectedProvince ? selectedProvince.name : '');
+    } else {
+      setProvinceName('');
     }
+    
+    onLocationChange({
+      province: selectedProvince ? selectedProvince.name : '',
+      district: '',
+      ward: '',
+      wardText: ''
+    });
   };
 
   const handleDistrictChange = (e) => {
     const selectedDistrictId = e.target.value;
+    const selectedDistrict = districts.find((district) => district.code.toString() === selectedDistrictId.toString());
+    
     setFieldValue('district', selectedDistrictId);
     setFieldValue('ward', '');
     setWards([]);
-
+    
     if (selectedDistrictId) {
-      fetchWards(selectedDistrictId);
+      setDistrictName(selectedDistrict ? selectedDistrict.name : '');
+    } else {
+      setDistrictName('');
     }
+    
+    onLocationChange({
+      province: provinceName,
+      district: selectedDistrict ? selectedDistrict.name : '',
+      ward: null,
+      wardText: ''
+    });
   };
 
+  
+
+  
+  
+
+  // const handleWardChange = (e) => {
+  //   const selectedWardId = e.target.value;
+  //   setFieldValue('ward', selectedWardId);
+  //   onLocationChange({
+  //     province: values.province,
+  //     district: values.district,
+  //     ward: selectedWardId,
+  //     wardText: values.ward,
+  //   });
+  // };
   const handleWardChange = (e) => {
-    const selectedWardId = e.target.value;
-    setFieldValue('ward', selectedWardId);
+    const selectedWardValue = e.target.value;
+    setFieldValue('ward', selectedWardValue);
+    setWardValue(selectedWardValue);
+
+    onLocationChange({
+      province: provinceName,
+      district: districtName,
+      ward: selectedWardValue,
+      wardText: ''
+    });
   };
+ 
 
   // Gửa địa chia qua conponent  EditAddressForm:
-  
+  // useEffect(() => {
+  //   fetchProvinces();
+  // }, []);
+
+  // const fetchProvinces = () => {
+  //   axios
+  //     .get('https://provinces.open-api.vn/api/')
+  //     .then((response) => {
+  //       setProvinces(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error);
+  //     });
+  // };
+
+  // const fetchDistricts = (provinceId) => {
+  //   axios
+  //     .get(`https://provinces.open-api.vn/api/p/${provinceId}?depth=2`)
+  //     .then((response) => {
+  //       const { districts } = response.data;
+  //       setDistricts(districts);
+  //       setWards([]); // Reset the wards when changing the district
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error);
+  //     });
+  // };
+
+  // const fetchWards = (districtId) => {
+  //   axios
+  //     .get(`https://provinces.open-api.vn/api/districts/${districtId}/wards`)
+  //     .then((response) => {
+  //       setWards(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error);
+  //     });
+  // };
+
+  // const handleProvinceChange = (e) => {
+  //   const selectedProvinceId = e.target.value;
+  //   setFieldValue('province', selectedProvinceId);
+  //   setFieldValue('district', '');
+  //   setFieldValue('ward', '');
+  //   setDistricts([]);
+  //   setWards([]);
+
+  //   if (selectedProvinceId) {
+  //     fetchDistricts(selectedProvinceId);
+  //   }
+  // };
+
+  // const handleDistrictChange = (e) => {
+  //   const selectedDistrictId = e.target.value;
+  //   setFieldValue('district', selectedDistrictId);
+  //   setFieldValue('ward', '');
+  //   setWards([]);
+
+  //   if (selectedDistrictId) {
+  //     fetchWards(selectedDistrictId);
+  //   }
+  // };
+
+  // const handleWardChange = (e) => {
+  //   const selectedWardId = e.target.value;
+  //   setFieldValue('ward', selectedWardId);
+  // };
 
   return (
     <div className='location_div'>
@@ -93,7 +228,12 @@ const Location = ({ values, setFieldValue }) => {
           onChange={handleProvinceChange}
         >
           <option value="">Chọn</option>
-          {provinces.map((province) => (
+          {/* {provinces.map((province) => (
+            <option key={province.code} value={province.code}>
+              {province.name}
+            </option>
+          ))} */}
+           {provinces && provinces.map((province) => (
             <option key={province.code} value={province.code}>
               {province.name}
             </option>
@@ -111,9 +251,14 @@ const Location = ({ values, setFieldValue }) => {
           disabled={!values.province}
         >
           <option value="">Chọn</option>
-          {districts.map((district) => (
+          {/* {districts.map((district) => (
             <option key={district.code} value={district.code}>
               {district.name}
+            </option>
+          ))} */}
+              {districts && districts.map((district) => (
+                <option key={district.code} value={district.code}>
+                {district.name}
             </option>
           ))}
         </select>
@@ -122,7 +267,9 @@ const Location = ({ values, setFieldValue }) => {
       
       <div>
         <label htmlFor="ward">Địa chỉ cụ thể (Đường, số nhà, thôn, Xã/Phường/Thị trấn):</label>
-        <TextField className='TextFieldWard' variant='outlined' multiline rows={3}></TextField>
+        <TextField className='TextFieldWard' variant='outlined' multiline rows={3}  value={values.ward}
+          onChange={handleWardChange}
+          disabled={!values.district}></TextField>
       </div>
 
       {/* <Field as={TextField} name='ward' variant='outlined' /> */}
